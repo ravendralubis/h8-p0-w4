49e235f7db283a5cbf522052539c4eda485c3243
ravendralubis <46861872+ravendralubis@users.noreply.github.com>
2019-02-21 23:42:07 +0700

    Add files via upload

--- checkAB.js
+++ checkAB.js
@@ -1,26 +1,16 @@
 function checkAB(num) {
-    var str = num.split('')
-    var arr = []
-
-    for(var i = 0; i < str.length; i++) {
-        if(str[i] !== ' ') {
-            arr.push(str[i])
-        }
-    }
-    for(var j = 0; j < arr.length; j++) {
-        if(arr[j] === 'a') {
-            for(var k = j; k <= (j+3); k++) {
-                if(arr[k] === 'b') {
-                    return true
-                }
-            }
+    for(var i = 0; i < num.length; i++) {
+        if(num[i] === 'a' && num[i+4] === 'b') {
+            return true
+        } else if(num[i] === 'b' && num[i+4] === 'a') {
+            return true
         }
     }
     return false
   }
   
   // TEST CASES
-  console.log(checkAB('lane borrowed')); // true
+  console.log(checkAB('lbne aorrowed')); // true
   console.log(checkAB('i am sick')); // false
   console.log(checkAB('you are boring')); // true
   console.log(checkAB('barbarian')); // true
