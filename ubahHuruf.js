a7bd3515ba6186e370e8dadc9c118c0bd6f678a3
ravendralubis <46861872+ravendralubis@users.noreply.github.com>
2019-02-21 22:22:09 +0700

    Add files via upload

--- ubahHuruf.js
+++ ubahHuruf.js
@@ -5,8 +5,13 @@ function ubahHuruf(kata) {
     for(var i = 0; i < kata.length; i++) {
         for(var j = 0; j < abjad.length; j++) {
             if(kata[i] === abjad[j]) {
-                result += abjad[j+1]
-            }
+                var abjadDepan = abjad[j+1]
+                if(abjadDepan === undefined) {
+                    result += abjad[0]
+                } else {
+                   result += abjad[j+1]
+                }
+            }   
         }
     }
     return result
@@ -15,7 +20,7 @@ function ubahHuruf(kata) {
       
   
   // TEST CASES
-  console.log(ubahHuruf('wow')); // xpx
+  console.log(ubahHuruf('woz')); // xpx
   console.log(ubahHuruf('developer')); // efwfmpqfs
   console.log(ubahHuruf('javascript')); // kbwbtdsjqu
   console.log(ubahHuruf('keren')); // lfsfo
